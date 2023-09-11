47e832515234cc891386ee56bac1de3dfc23d3e0
Vennisumi <144649839+Vennisumi@users.noreply.github.com>
2023-09-11 11:40:29 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,16 @@
+#leep year
+def isleapyear(year):
+ if(year%4==0 and year %100!=0)or year%400==0:
+   return True
+ else:
+   return False
+   year=int(input("Enter a value:"))
+if isleap (year):
+   print('{} is a Leap year.'.format(year))
+ else:
+   print('{} is not a Leap year.'.
+     format(year)
+    
+ 
+    
+
